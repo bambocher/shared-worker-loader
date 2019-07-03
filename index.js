f41c15cdb003542cc590e5606a3c37fe33d1e5ba
Dmitry Prazdnichnov <dp@bambucha.org>
2019-07-03 12:48:57 +0300

    Linter

--- index.js
+++ index.js
@@ -1,52 +1,63 @@
-var WebWorkerTemplatePlugin = require('webpack/lib/webworker/WebWorkerTemplatePlugin');
-var SingleEntryPlugin = require('webpack/lib/SingleEntryPlugin');
-var path = require('path');
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-names */
+const WebWorkerTemplatePlugin = require('webpack/lib/webworker/WebWorkerTemplatePlugin');
+const SingleEntryPlugin = require('webpack/lib/SingleEntryPlugin');
+const path = require('path');
 
-var loaderUtils = require('loader-utils');
-module.exports = function() {};
-module.exports.pitch = function(request) {
-  if (!this.webpack)
+const loaderUtils = require('loader-utils');
+
+module.exports = function () {};
+module.exports.pitch = function (request) {
+  if (!this.webpack) {
     throw new Error('Only usable with webpack');
-  var callback = this.async();
-  var query = loaderUtils.parseQuery(this.query);
-  var outputOptions = {
+  }
+
+  const callback = this.async();
+  const query = loaderUtils.parseQuery(this.query);
+  const output = {
     filename: '[hash].sharedworker.js',
     chunkFilename: '[id].[hash].sharedworker.js',
-    namedChunkFilename: null
+    namedChunkFilename: null,
   };
+
   if (this.options && this.options.worker && this.options.worker.output) {
-    for (var name in this.options.worker.output) {
-      outputOptions[name] = this.options.worker.output[name];
+    for (const name in this.options.worker.output) {
+      output[name] = this.options.worker.output[name];
     }
   }
-  var workerCompiler = this._compilation.createChildCompiler('sharedworker', outputOptions);
-  workerCompiler.apply(new WebWorkerTemplatePlugin(outputOptions));
-  workerCompiler.apply(new SingleEntryPlugin(this.context, '!!' + request, 'main'));
+
+  const workerCompiler = this._compilation.createChildCompiler('sharedworker', output);
+
+  workerCompiler.apply(new WebWorkerTemplatePlugin(output));
+  workerCompiler.apply(new SingleEntryPlugin(this.context, `!!${request}`, 'main'));
+
   if (this.options && this.options.worker && this.options.worker.plugins) {
-    this.options.worker.plugins.forEach(function(plugin) {
+    this.options.worker.plugins.forEach((plugin) => {
       workerCompiler.apply(plugin);
     });
   }
-  var subCache = 'subcache ' + __dirname + ' ' + request;
-  workerCompiler.plugin('compilation', function(compilation) {
+
+  const subCache = `subcache ${__dirname} ${request}`;
+  workerCompiler.plugin('compilation', (compilation) => {
     if (compilation.cache) {
-      if (!compilation.cache[subCache])
+      if (!compilation.cache[subCache]) {
         compilation.cache[subCache] = {};
+      }
       compilation.cache = compilation.cache[subCache];
     }
   });
-  workerCompiler.runAsChild(function(err, entries, compilation) {
+
+  workerCompiler.runAsChild((err, entries, compilation) => {
     if (err) return callback(err);
     if (entries[0]) {
-      var workerFile = entries[0].files[0];
-      var constructor = 'new SharedWorker(__webpack_public_path__ + ' + JSON.stringify(workerFile) + ', name)';
+      const workerFile = entries[0].files[0];
+      let constructor = `new SharedWorker(__webpack_public_path__ + ${JSON.stringify(workerFile)}, name)`;
       if (query.inline) {
-        constructor = 'require(' + JSON.stringify('!!' + path.join(__dirname, 'createInlineWorker.js')) + ')(' +
-          JSON.stringify(compilation.assets[workerFile].source()) + ', __webpack_public_path__ + ' + JSON.stringify(workerFile) + ', name)';
+        constructor = `require(${JSON.stringify(`!!${path.join(__dirname, 'createInlineWorker.js')}`)})(${
+          JSON.stringify(compilation.assets[workerFile].source())}, __webpack_public_path__ + ${JSON.stringify(workerFile)}, name)`;
       }
-      return callback(null, 'module.exports = function(name) {\n\treturn ' + constructor + ';\n};');
-    } else {
-      return callback(null, null);
+      return callback(null, `module.exports = function(name) {\n\treturn ${constructor};\n};`);
     }
+    return callback(null, null);
   });
 };
